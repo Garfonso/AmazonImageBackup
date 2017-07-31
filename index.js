faa71bf4d8f418b8383c04289d7ddc1e37c8f0bc
Achim Königs <garfonso@mobo.info>
2017-07-31 14:19:36 +0200

    Store access token on hdd.

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const https = require("https");
 const url = require("url");
 const auth = require("./auth.json");
 const querystring = require("querystring");
+const fs = require("fs");
 
 //use someone elses client secret, because amazon won't give out new secrects, currently. :-(
 const refreshProxyUrl = "https://drivesink.appspot.com/refresh";
-
 const debugging = true;
+const accessTokenPath = "./auth_access.json";
 
 function debug(...msgs) {
     if (debugging) {
@@ -23,26 +24,26 @@ function debug(...msgs) {
     }
 }
 
-function refreshToken() {
+const readFilePromise = NodePromise.denodeify(fs.readFile);
+const writeFilePromise = NodePromise.denodeify(fs.writeFile);
+function requestPromise(options, postData, decode) {
     let promise = new NodePromise(function resolver(resolve, reject) {
-        let options = url.parse(refreshProxyUrl);
-        options.method = "POST";
-        let postData = querystring.stringify({refresh_token: auth.refresh_token});
-        options.headers = { "Content-Length": Buffer.byteLength(postData)};
-
-        debug("Sending request", options);
+        //debug("Sending request", options);
         let req = https.request(options, function callback(res) {
             let data = "";
             res.setEncoding("utf8");
-            debug("Headers:", res.headers);
+            //debug("Headers:", res.headers);
             res.on("data", function (chunk) { data += chunk; });
             res.on("end", function () {
-                debug("Body: ", data);
+                //debug("Body: ", data);
                 if (res.statusCode === 200) {
-                    resolve(JSON.parse(data));
+                    if (decode) {
+                        resolve(JSON.parse(data));
+                    } else {
+                        resolve(data);
+                    }
                 } else {
-
-                    reject("Status code not 200: " + res.statusCode);
+                    reject({msg: "Status code not 200: " + res.statusCode, data: data});
                 }
             });
         });
@@ -53,7 +54,45 @@ function refreshToken() {
     return promise;
 }
 
+let authHeader;
+function refreshToken(realRefresh) {
+    let promise;
+    if (!realRefresh) {
+        promise = readFilePromise(accessTokenPath, "utf8");
+
+        promise = promise.then(function gotToken(data) {
+            let token = JSON.parse(data);
+            debug("Got token from file");
+            authHeader = "Bearer " + token.access_token;
+            return token;
+        }, function needToRefresh() {
+            return refreshToken(true);
+        });
+    } else {
+        let options = url.parse(refreshProxyUrl);
+        options.method = "POST";
+        let postData = querystring.stringify({refresh_token: auth.refresh_token});
+        options.headers = {"Content-Length": Buffer.byteLength(postData)};
+
+        promise = requestPromise(options, postData, true);
+
+        promise = promise.then(function storeToken(token) {
+            debug("Got token from refresh.");
+            authHeader = "Bearer " + token.access_token;
+            return writeFilePromise(accessTokenPath, JSON.stringify(token, null, 4), "utf8");
+        });
+    }
+
+    return promise;
+}
+
+//first get access_token:
 let promise = refreshToken();
+
+/*promise = promise.then(function getRootNode() {
+    let options = "";
+    //requestPromise()
+});*/
 
 promise = promise.then(function allgood(result) {
     console.log("All good:", result);
